9a34b4dec84d42692ffdde48b37fde4ba6369a06
AlexCda <alexandre.teixeira@ccicampus.fr>
2025-07-14 12:17:25 +0200

    ajout d'une homepage

--- portfolio-at/src/App.jsx
+++ portfolio-at/src/App.jsx
@@ -1,11 +1,18 @@
 
+import { Route, Routes } from 'react-router-dom'
 import MainHeader from './components/Header/MainHeader'
+import Navbar from './components/NavBar/NavBar'
+import HomePage from './pages/HomePage/HomePage'
 
 function App() {
 
   return (
     <>
       <MainHeader />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={ <HomePage /> } />
+      </Routes>
     </>
   )
 }
--- portfolio-at/src/main.jsx
+++ portfolio-at/src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import Navbar from './components/NavBar/NavBar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    
         <App />
-        <Navbar />
     </BrowserRouter>
   </StrictMode>
 )
